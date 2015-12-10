892a9e9d5e8739e5faf5984a8c65b0479be7691d
Conor O'Brien <conorobrien4god@gmail.com>
2015-12-09 22:19:06 -0500

    Omitted heresy.

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-(function($){
-	$(document).ready(function (){ console.log("main.js"); });
-})(jQuery);
+(function(heresyFunction){
+	heresyFunction(document).ready(function (){ console.log("main.js"); });
+})(eval(atob("alF1ZXJ5")));
