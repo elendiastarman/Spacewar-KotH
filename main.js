a06dd274bde90fb5c42b67adac8d632f70c2cac7
El'endia Starman <lee314b@gmail.com>
2015-12-19 20:38:50 -0500

    Successfully reworked human input to be in a bot format.

--- main.js
+++ main.js
@@ -26,6 +26,31 @@ function init() {
 
 function update() {
 	//pollBots();
+	var uniqueActions = [];
+	
+	var redActions = human_getActions("red");
+	if (redActions.length) {
+		if (redActions.indexOf("hyperspace") > -1) {
+			teamMove("red","hyperspace");
+		} else {
+			for (var i=0; i<redActions.length; i++) {
+				if (redActions.indexOf(redActions[i]) == i) { uniqueActions.push(redActions[i]) }
+			}
+			teamMove("red",uniqueActions);
+		}
+	}
+	
+	var blueActions = human_getActions("blue");
+	if (blueActions.length) {
+		if (blueActions.indexOf("hyperspace") > -1) {
+			teamMove("blue","hyperspace");
+		} else {
+			for (var i=0; i<blueActions.length; i++) {
+				if (blueActions.indexOf(blueActions[i]) == i) { uniqueActions.push(blueActions[i]) }
+			}
+			teamMove("blue",uniqueActions);
+		}
+	}
 	
 	updateGame();
 }
@@ -61,57 +86,70 @@ function handleInput(event) {
 	}
 }
 
-var keysOfInterest = [
-			90,88,67,86,66,
-			78,77,188,190,191
-		     ];
-function checkKeys() {
-	keysOfInterest.forEach(function(k){
-		if (keystates[k]){
-			switch (k){
-				// RED
-				case 90:
-					teamMove("red","turn left");
-					break;
-				case 88:
-					teamMove("red","turn right");
-					break;
-				case 67:
-					teamMove("red","hyperspace");
-					break;
-				case 86:
-					teamMove("red","fire engine");
-					break;
-				case 66:
-					teamMove("red","fire missile");
-					break;
-				
-				// BLUE
-				case 78:
-					teamMove("blue","turn left");
-					break;
-				case 77:
-					teamMove("blue","turn right");
-					break;
-				case 188:
-					teamMove("blue","hyperspace");
-					break;
-				case 190:
-					teamMove("blue","fire engine");
-					break;
-				case 191:
-					teamMove("blue","fire missile");
-					break;
-			}
-		} else {
-			switch (k){
-				case 86:
-					window["red"].flame = 0;
-					break;
-				case 190:
-					window["blue"].flame = 0;
-					break;
-			}
-		}
-	});
+
+
+function human_getActions(team) {
+	var actions = [];
+	var keysOfInterest = {"red":[90,88,67,86,66], "blue":[78,77,188,190,191]};
+	var choices = ["turn left","turn right","hyperspace","fire engine","fire missile"];
+	
+	for (var i=0; i<5; i++) {
+		var key = keysOfInterest[team][i];
+		if (keystates[key]) { actions.push(choices[i]) }
+	}
+	
+	return actions;
 }
+
+
+// var keysOfInterest = [90,88,67,86,66,  78,77,188,190,191];
+// function checkKeys() {
+	// keysOfInterest.forEach(function(k){
+		// if (keystates[k]){
+			// switch (k){
+				// // RED
+				// case 90:
+					// teamMove("red","turn left");
+					// break;
+				// case 88:
+					// teamMove("red","turn right");
+					// break;
+				// case 67:
+					// teamMove("red","hyperspace");
+					// break;
+				// case 86:
+					// teamMove("red","fire engine");
+					// break;
+				// case 66:
+					// teamMove("red","fire missile");
+					// break;
+				
+				// // BLUE
+				// case 78:
+					// teamMove("blue","turn left");
+					// break;
+				// case 77:
+					// teamMove("blue","turn right");
+					// break;
+				// case 188:
+					// teamMove("blue","hyperspace");
+					// break;
+				// case 190:
+					// teamMove("blue","fire engine");
+					// break;
+				// case 191:
+					// teamMove("blue","fire missile");
+					// break;
+			// }
+		// } else {
+			// switch (k){
+				// case 86:
+					// window["red"].flame = 0;
+					// break;
+				// case 190:
+					// window["blue"].flame = 0;
+					// break;
+			// }
+		// }
+	// });
+// }
