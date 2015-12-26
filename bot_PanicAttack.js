e2053ffccb4e3d9bbc453723bb2449588d1c8ad4
El'endia Starman <lee314b@gmail.com>
2015-12-26 15:43:07 -0500

    Updated PanicAttack bot.

--- bot_PanicAttack.js
+++ bot_PanicAttack.js
@@ -7,7 +7,14 @@ function PanicAttack_setup(team) {
 function PanicAttack_getActions(gameInfo, botVars) {
     var actions = [];
     actions.push("fire engine");
-    if (Math.random()>0.5) {
+    if(botVars.color == "red") {
+        var opponentAlive = gameInfo.blue_alive;
+    }
+    else if(botVars.color == "blue") {
+        var opponentAlive = gameInfo.red_alive;
+    }
+
+    if ((Math.random()>0.5)&&opponentAlive) {
         actions.push("fire missile");
     }
 
