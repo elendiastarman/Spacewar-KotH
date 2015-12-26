7da1e15e2215b683ae84eaf8ae77e585fc1ee2af
El'endia Starman <lee314b@gmail.com>
2015-12-26 15:39:55 -0500

    Updated OrbitBot.

--- bot_OrbitBot.js
+++ bot_OrbitBot.js
@@ -13,8 +13,32 @@ function OrbitBot_getActions(gameInfo, botVars) {
     return gameInfo[botVars.color + "_" + name];
   }
 
+  function getEnemyVar(name) {
+    var eColor;
+    if (botVars.color == 'blue') {
+        eColor = 'red';
+    } else {
+        eColor = 'blue';
+    }
+    return gameInfo[eColor + "_" + name];
+  }
+
   function distance(x1, y1, x2, y2) {
     return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+  }
+
+  function toroidDistance(x1, y1, x2, y2) {
+    dx = Math.abs(x1 - x2);
+        while (dx > gameInfo.fieldWidth) {
+        dx -= gameInfo.fieldWidth;
+    }
+    dx = Math.min(dx, gameInfo.fieldWidth - dx);
+    dy = Math.abs(y1 - y2);
+        while (dx > gameInfo.fieldHeight) {
+        dx -= gameInfo.fieldHeight;
+    }
+    dy = Math.min(dy, gameInfo.fieldHeight - dy);
+    return Math.sqrt(dx*dx+dy*dy);
   }
 
   function angleDistance(theta1, theta2) {
@@ -36,6 +60,16 @@ function OrbitBot_getActions(gameInfo, botVars) {
     return degrees / 180 * Math.PI;
   }
 
+  function cap(x, y, limit) {
+    var r = x*x+y*y;
+    if (r < limit * limit) {
+        r = Math.sqrt(r);
+        x = x * r / limit;
+      y = y * r / limit;
+    }
+    return [x,y];
+  }
+
   var shape = getVar('shape');
 
   if (shape != 'nose only') {
@@ -44,13 +78,60 @@ function OrbitBot_getActions(gameInfo, botVars) {
       sunY = gameInfo.sun_y,
       sunG = gameInfo.gravityStrength;
 
-    var x = getVar("x") - sunX,
-      y = sunY - getVar("y"),
-      vx = getVar("xv"),
-      vy = -getVar("yv");
+    function desirability(x, y, vx, vy) {     //Borrowed from a useless bot.
+      var lowest = distance(x, y, sunX, sunY) - 5;
+      var missiles = gameInfo.missiles;
+      for (var i = 0; i < missiles.length; i++) {
+        var mx = missiles[i].x + missiles[i].xv / 2;
+        var my = missiles[i].y + missiles[i].yv / 2;
+        lowest = Math.min(lowest, toroidDistance(x, y, mx, my) - distance(0, 0, missiles[i].xv, missiles[i].yv));
+      }
+      return lowest - 16;
+    }
 
-    var r = Math.sqrt(x * x + y * y);
-    var theta = Math.atan(y / x);
+    var x = getVar("x"),
+      y = getVar("y"),
+      vx = getVar("xv"),
+      vy = getVar("yv");
+
+    function desirabilityByAcceleration(ax, ay) {//Borrowed from a useless bot.
+        var x1 = x,
+            y1 = y,
+          vx1 = vx,
+          vy1 = vy;
+      var speed = distance(0,0,vx1,vy1);
+      var limit = Math.max(gameInfo.speedLimit, speed);
+
+      vx1 += ax;
+      vy1 += ay;
+      var temp = cap(vx1, vy1, limit);
+      vx1 = temp[0];
+      vy1 = temp[1];
+
+
+      var dx = x1 - sunX;
+      var dy = y1 - sunY;
+      var dis = Math.sqrt(dx*dx+dy*dy);
+      if (dis > 5){
+        var force = sunG / (dis * dis);
+      } else {
+        var force = sunG /5;
+      }
+      vx1 -= force*dx/dis;
+      vy1 -= force*dy/dis;
+
+      var temp = cap(vx1, vy1, 40);
+      vx1 = temp[0];
+      vy1 = temp[1];
+
+      x1 += vx1;
+      y1 += vy1;
+
+      return desirability(x1, y1, vx1, vy1);
+    }
+
+    var r = distance(sunX, sunY, x, y);
+    var theta = Math.atan((y - sunY) / (x - sunX));
 
     var sunA = sunG/r/r,
             sunAx = -Math.cos(theta) * sunA,
@@ -71,9 +152,9 @@ function OrbitBot_getActions(gameInfo, botVars) {
     if (dax < 0) {
         dAngle += Math.PI;
     }
-    var cAngle = toRad(90 - getVar('rot'));
-    var dLeft = angleDistance(cAngle + toRad(broken ? 2.5 : 5), dAngle);
-    var dRight = angleDistance(cAngle - toRad(broken ? 2.5 : 5), dAngle);
+    var cAngle = toRad(getVar('rot') - 90);
+    var dLeft = angleDistance(cAngle - toRad(broken ? 2.5 : 5), dAngle);
+    var dRight = angleDistance(cAngle + toRad(broken ? 2.5 : 5), dAngle);
     var dNeither = angleDistance(cAngle, dAngle);
     if (dLeft < dRight && dLeft < dNeither) {
       actions.push('turn left');
@@ -84,8 +165,17 @@ function OrbitBot_getActions(gameInfo, botVars) {
     var cax = Math.cos(cAngle) * (broken ? .15 : .3);
     var cay = Math.sin(cAngle) * (broken ? .15 : .3);
 
+    var ax = 0;
+    var ay = 0;
+
     if (distance(cax, cay, dax, day) < distance(0, 0, dax, day)) {
       actions.push('fire engine');
+      ax = cax;
+      ay = cay;
+    }
+
+    if (desirabilityByAcceleration(ax, ay) <= 16) {
+        actions.push('hyperspace');
     }
 
   }
